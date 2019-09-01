447d17c1d58cd544c3805e968475806519d02f01
roshni mansuri <roshni7.rockersinfo@gmail.com>
2019-09-01 16:50:47 +0530

    Combination of plugins with customization to get slide in contact form

---
+++ slide-in-form/js/admin.js
@@ -0,0 +1,12 @@
+(function ( $ ) {
+	"use strict";
+
+	$(function () {
+
+		$( document ).ready(function() {	
+			$('#form_header_color').wpColorPicker();
+			$('#form_header_border_color').wpColorPicker();
+		});
+	});
+
+}(jQuery));
